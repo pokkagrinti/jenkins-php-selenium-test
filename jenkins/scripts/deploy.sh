b0f68e33e274c387016d140606aab59ba0b4e074
pokkagrinti <60540228+pokkagrinti@users.noreply.github.com>
2023-11-14 17:11:53 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
-
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker network create --subnet=172.18.0.0/16 test-network
+docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html --ip 172.18.0.2 php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
